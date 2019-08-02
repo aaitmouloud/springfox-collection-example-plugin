56c64723b5b5929b8ba1d46e5c89cf3c1a015c49
aaitmouloud <a.aitmouloud@outlook.com>
2019-08-02 14:15:12 +0000

    working tests

--- src/test/java/springfox/collection/example/plugins/CollectionExamplePluginTest.java
+++ src/test/java/springfox/collection/example/plugins/CollectionExamplePluginTest.java
@@ -4,7 +4,6 @@ import io.swagger.annotations.ApiModelProperty;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import springfox.documentation.builders.ModelPropertyBuilder;
@@ -14,71 +13,77 @@ import springfox.documentation.spi.schema.contexts.ModelPropertyContext;
 import java.lang.reflect.AnnotatedElement;
 import java.util.List;
 
+import static com.google.common.base.Optional.of;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class CollectionExamplePluginTest {
-
+    
     private static final String EXAMPLE = "my example";
-
+    
     private CollectionExamplePlugin collectionExamplePlugin;
-
-    @InjectMocks
+    
+    @Mock
     private ModelPropertyContext modelPropertyContext;
-
+    
     @Mock
     private ModelPropertyBuilder builder;
-
+    
     @Mock
     private AnnotatedElement annotatedElement;
-
+    
     @Mock
     private ApiModelProperty apiModelProperty;
-
+    
     @BeforeEach
     void setUp() {
         collectionExamplePlugin = new CollectionExamplePlugin();
     }
-
-
+    
+    
     @Test
     void doesNothingWhenNotApiModelBuilderAnnotation() {
+        when(modelPropertyContext.getAnnotatedElement()).thenReturn(of(annotatedElement));
         when(annotatedElement.isAnnotationPresent(ApiModelProperty.class)).thenReturn(false);
-
+        
         collectionExamplePlugin.apply(modelPropertyContext);
-
+        
         verifyZeroInteractions(builder);
     }
-
-
+    
+    
     @Test
     void doesNothingWhenNotIsNotACollection() {
+        when(modelPropertyContext.getAnnotatedElement()).thenReturn(of(annotatedElement));
+        when(modelPropertyContext.getBuilder()).thenReturn(builder);
         when(annotatedElement.isAnnotationPresent(ApiModelProperty.class)).thenReturn(true);
         when(annotatedElement.getAnnotation(ApiModelProperty.class)).thenReturn(apiModelProperty);
-
+        
         final ModelProperty modelPropertyMock = mock(ModelProperty.class);
         when(builder.build()).thenReturn(modelPropertyMock);
         when(modelPropertyMock.getQualifiedType()).thenReturn(String.class.getCanonicalName());
-
+        
         collectionExamplePlugin.apply(modelPropertyContext);
-
+        
         verifyZeroInteractions(builder);
     }
-
+    
     @Test
     void fixesExampleWhenIsACollection() {
+        when(modelPropertyContext.getAnnotatedElement()).thenReturn(of(annotatedElement));
+        when(modelPropertyContext.getBuilder()).thenReturn(builder);
         when(annotatedElement.isAnnotationPresent(ApiModelProperty.class)).thenReturn(true);
         when(annotatedElement.getAnnotation(ApiModelProperty.class)).thenReturn(apiModelProperty);
-
+        
         final ModelProperty modelPropertyMock = mock(ModelProperty.class);
         when(builder.build()).thenReturn(modelPropertyMock);
         when(modelPropertyMock.getQualifiedType()).thenReturn(List.class.getCanonicalName());
-
+        
         when(apiModelProperty.example()).thenReturn(EXAMPLE);
-
+        
         collectionExamplePlugin.apply(modelPropertyContext);
-
+        
         verify(builder).example(new Object[]{EXAMPLE});
     }
-
+    
 }
